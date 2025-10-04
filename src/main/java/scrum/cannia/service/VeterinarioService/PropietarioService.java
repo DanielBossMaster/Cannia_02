c197b26643f2a611593977a5d418e701635a5f5b
DanielBossMaster <sena.danielboss@gmail.com>
2025-10-04 15:59:37 -0500

    Estoy con lo de cambiar estado

---
+++ src/main/java/scrum/cannia/service/VeterinarioService/PropietarioService.java
@@ -0,0 +1,23 @@
+package scrum.cannia.service.VeterinarioService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import scrum.cannia.model.PropietarioModel;
+import scrum.cannia.repository.PropietarioRepository;
+
+
+@Service
+public class PropietarioService {
+
+    @Autowired
+    private PropietarioRepository propietarioRepository;
+
+    public void eliminarPropietario(Long id) {
+        PropietarioModel propietario = propietarioRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Propietario no encontrado"));
+
+        propietario.setEstado(false);
+        propietarioRepository.save(propietario);
+    }
+}
+
