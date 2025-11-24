0e67c38e615a1b3191babcdcad62bb40ed56b548
Maryi-G <grajalesmaryi2426@gmail.com>
2025-11-24 15:37:11 -0500

    anexo de los findby de fundacion al final

--- src/main/java/scrum/cannia/repository/MascotaRepository.java
+++ src/main/java/scrum/cannia/repository/MascotaRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import scrum.cannia.model.MascotaModel;
 import scrum.cannia.model.PropietarioModel;
+import scrum.cannia.model.FundacionModel;
 
 import java.util.List;
 
@@ -12,4 +13,9 @@ public interface MascotaRepository extends JpaRepository<MascotaModel, Long> {
     List<MascotaModel> findByPropietarioId(Long idPropietario);
 
     List<MascotaModel> findByPropietario(PropietarioModel propietario);
+
+    // FundacioN
+    List<MascotaModel> findByFundacionId(Long idFundacion);
+
+    List<MascotaModel> findByFundacion(FundacionModel fundacion);
 }
