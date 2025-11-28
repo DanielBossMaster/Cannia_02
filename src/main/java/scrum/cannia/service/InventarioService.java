ebbd451782d85eb82f275d2933a9ef9750803c97
lizeth <lizeth11103@gmail.com>
2025-11-28 07:11:31 -0500

    Inventarios

---
+++ src/main/java/scrum/cannia/service/InventarioService.java
@@ -0,0 +1,41 @@
+package scrum.cannia.service;
+
+import scrum.cannia.model.InventarioModel;
+import scrum.cannia.repository.InventarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class InventarioService {
+
+    @Autowired
+    private InventarioRepository inventarioRepository;
+
+    public List<InventarioModel> obtenerTodoInventario() {
+        return inventarioRepository.findAll();
+    }
+
+    public InventarioModel guardarInventario(InventarioModel inventario) {
+        inventario.setFechaActualizacion(java.time.LocalDate.now());
+        return inventarioRepository.save(inventario);
+    }
+
+    public Map<String, Object> obtenerDatosGraficos() {
+        Map<String, Object> datos = new HashMap<>();
+
+        // Alertas de stock
+        List<InventarioModel> stockCritico = inventarioRepository.findByStockActualLessThan(10);
+        List<InventarioModel> stockNormal = inventarioRepository.findByStockActualBetween(10, 50);
+        List<InventarioModel> stockAlto = inventarioRepository.findByStockActualBetween(51, 100);
+
+        datos.put("stockCritico", stockCritico);
+        datos.put("stockNormal", stockNormal);
+        datos.put("stockAlto", stockAlto);
+        datos.put("totalProductos", inventarioRepository.count());
+
+        return datos;
+    }
+}
