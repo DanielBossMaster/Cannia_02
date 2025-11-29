69b6f624039fbeb63c7c41444865b424608db74f
Maryi-G <grajalesmaryi2426@gmail.com>
2025-11-29 17:06:40 -0500

    Agregue método para obtener solo fundaciones activas

--- src/main/java/scrum/cannia/repository/FundacionRepository.java
+++ src/main/java/scrum/cannia/repository/FundacionRepository.java
@@ -2,8 +2,10 @@ package scrum.cannia.repository;
 import scrum.cannia.model.FundacionModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface FundacionRepository extends JpaRepository<FundacionModel, Long> {
+    List<FundacionModel> findByEstadoTrue(); // para ver solo fundaciones activas
 }
 
