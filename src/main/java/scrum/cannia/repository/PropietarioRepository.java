834bc56541e68a7c1f5b301976a51789a89bc827
DanielBossMaster <sena.danielboss@gmail.com>
2025-12-02 02:55:16 -0500

    añado metodo para encontrar todos los correos para enviar correos

--- src/main/java/scrum/cannia/repository/PropietarioRepository.java
+++ src/main/java/scrum/cannia/repository/PropietarioRepository.java
@@ -1,6 +1,7 @@
 package scrum.cannia.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import scrum.cannia.model.PropietarioModel;
 
 import java.util.List;
@@ -9,5 +10,9 @@ public interface PropietarioRepository extends JpaRepository<PropietarioModel,Lo
 List<PropietarioModel> findByEstadoTrue();
     PropietarioModel findByNumDoc(String numDoc);
     PropietarioModel findByUsuario_IdUsuario(Long idUsuario);
+
+
+    @Query("SELECT p.correoPro FROM PropietarioModel p WHERE p.correoPro IS NOT NULL")
+    List<String> obtenerCorreosDePropietarios();
 }
 
