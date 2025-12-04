262e2d685b752c16784bf2562f35b5c11040f14d
lizeth <lizeth11103@gmail.com>
2025-12-04 01:19:49 -0500

    se mueve de lugar a la carpeta del inventrio y se agregan nuevas rutas, ya sirve el guardar

--- src/main/java/scrum/cannia/controller/ServicioController.java
+++ src/main/java/scrum/cannia/controller/ServicioController.java
@@ -1,15 +1,85 @@
 package scrum.cannia.controller;
 
-
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import scrum.cannia.model.ServicioModel;
+import scrum.cannia.model.UsuarioModel;
+
+import scrum.cannia.repository.ServicioRepository;
+
+import scrum.cannia.service.ServicioService;
 
 @Controller
-@RequestMapping("/servicios")
+@RequestMapping("/inventario/servicios")
 public class ServicioController {
 
+    private final ServicioRepository servicioRepository;
+
+    public ServicioController(
+            ServicioRepository servicioRepository) {
+
+        this.servicioRepository = servicioRepository;
+    }
+
+        @Autowired
+        private ServicioService servicioService;
+
+        // ============================================
+        //   LISTAR INVENTARIO Y MOSTRAR FORMULARIOS
+        // ============================================
+        @GetMapping
+        public String servicio(Model model, HttpSession session) {
+            // Obtener el usuario en sesión
+            UsuarioModel usuario = (UsuarioModel) session.getAttribute("usuario");
+            if (usuario == null) {
+                return "redirect:/login";  // Seguridad
+            }
+
+            model.addAttribute("servicio", new ServicioModel());
+            model.addAttribute("servicios", servicioService.listarTodos());
+
+            return "Inventario/Servicio";
+
+        }
+
+    // ============================================
+    //         GUARDAR PRODUCTO
+    // ============================================
+
+    @PostMapping("/guardar")
+    public String guardar(@Validated @ModelAttribute ServicioModel servicio,
+                          BindingResult br,
+                          HttpSession session,
+                          Model model
+    ) {
+
+
+        // Obtener el usuario en sesión
+        UsuarioModel usuario = (UsuarioModel) session.getAttribute("usuario");
+        if (usuario == null) {
+            return "redirect:/login";  // Seguridad
+        }
+
+        // Validación del formulario
+        if (br.hasErrors()) {
+            model.addAttribute("mensaje", "Error al insertar los datos.");
+            model.addAttribute("servicio", new ServicioModel());
+            model.addAttribute("servicios", servicioService.listarTodos());
+            return "/Inventario/Servicio";
+
+        }
+
+        servicioRepository.save(servicio);
+
+        return "redirect:/inventario/servicios";
+        // ruta de la paginba, para que vuelva a la misma pagina
+    }
+    }
 //    @Autowired
 //    private ServicioService servicioService;
 //
@@ -43,4 +113,3 @@ public class ServicioController {
 //        servicioService.eliminarServicioLogicamente(id);
 //        return "redirect:/inventario";
 //    }
-}
