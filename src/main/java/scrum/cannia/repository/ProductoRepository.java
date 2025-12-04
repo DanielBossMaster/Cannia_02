28f28fecf0a621433748530657d948f6bfed5420
DanielBossMaster <sena.danielboss@gmail.com>
2025-12-04 05:26:22 -0500

    Se trabaja en la funcionalidad de la barra de busqeuda de productos

--- src/main/java/scrum/cannia/repository/ProductoRepository.java
+++ src/main/java/scrum/cannia/repository/ProductoRepository.java
@@ -4,10 +4,13 @@ import scrum.cannia.model.ProductoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductoRepository extends JpaRepository<ProductoModel, Integer> {
     List<ProductoModel> findByEstadoTrue(); // Solo productos activos
+    Optional<ProductoModel> findByNombre(String nombre);
+    List<ProductoModel> findByNombreContainingIgnoreCaseOrDescripcionContainingIgnoreCase(String nombre, String descripcion);
+}
 //    List<ProductoModel> findByPublicadoTrue(); // Solo productos publicados
 
-}
