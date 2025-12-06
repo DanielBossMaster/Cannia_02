1136cb8cd310d7b9ce03d2e4df3d5020ad8222f4
DanielBossMaster <sena.danielboss@gmail.com>
2025-12-06 06:45:08 -0500

    Estaba trabajando en la edicion de producto para agregar foto pero aun no acabo

--- src/main/java/scrum/cannia/repository/FacturaRepository.java
+++ src/main/java/scrum/cannia/repository/FacturaRepository.java
@@ -4,5 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import scrum.cannia.model.FacturaModel;
 
+import java.util.List;
+
 public interface FacturaRepository  extends JpaRepository<FacturaModel, Long> {
+    List<FacturaModel> findByVeterinaria_IdOrderByFechaEmisionDesc(Integer veterinariaId);
+
 }
