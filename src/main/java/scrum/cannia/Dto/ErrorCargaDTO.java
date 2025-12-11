ea43a53b8dbfc04763fc0cf47966c90bbbe47244
JhonS <jhonsuarezbasto@gmail.com>
2025-12-11 02:40:21 -0500

    Se crea DTO para errores

---
+++ src/main/java/scrum/cannia/Dto/ErrorCargaDTO.java
@@ -0,0 +1,15 @@
+package scrum.cannia.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorCargaDTO {
+
+    private int fila;
+    private String mensaje;
+
+}
