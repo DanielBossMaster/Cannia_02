ce458ab9c31e213e3f5b901f2b6afdfed4232055
Daniel <sena.danielboss@gmail.com>
2026-04-06 00:17:28 -0500

    agenda

--- src/main/java/scrum/cannia/service/AgendaService.java
+++ src/main/java/scrum/cannia/service/AgendaService.java
@@ -1,23 +1,65 @@
 package scrum.cannia.service;
 
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import scrum.cannia.model.AgendaModel;
 import scrum.cannia.model.PropietarioModel;
+import scrum.cannia.model.ServicioModel;
+import scrum.cannia.model.VeterinariaModel;
 import scrum.cannia.repository.AgendaRepository;
+import scrum.cannia.repository.ServicioRepository;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
+@AllArgsConstructor
 @Service
 public class AgendaService {
 
-    @Autowired
-    private AgendaRepository agendaRepository;
+    private final AgendaRepository agendaRepository;
+    private final ServicioRepository servicioRepository;
 
-    public List<AgendaModel> citasPorPropietario (PropietarioModel propietario){
-    return agendaRepository.findByPropietario(propietario);
+
+    public void agendarServicio(
+            Integer servicioId,
+            LocalDate fecha,
+            LocalTime hora,
+            PropietarioModel propietario
+    ) {
+
+        ServicioModel servicio = servicioRepository.findById(servicioId)
+                .orElseThrow(() -> new RuntimeException("Servicio no encontrado"));
+
+        VeterinariaModel veterinaria = servicio.getVeterinaria();
+
+        boolean ocupado = agendaRepository.existsByServicioAndFechaAndHoraAndEstado(
+                servicio,
+                fecha,
+                hora,
+                "AGENDADA"
+        );
+
+        if (ocupado) {
+            throw new RuntimeException("HORARIO_OCUPADO");
+        }
+
+        AgendaModel agenda = new AgendaModel();
+        agenda.setServicio(servicio);
+        agenda.setVeterinaria(veterinaria);
+        agenda.setPropietario(propietario);
+        agenda.setFecha(fecha);
+        agenda.setHora(hora);
+        agenda.setEstado("AGENDADA");
+
+        agendaRepository.save(agenda);
+    }
+
+    public List<AgendaModel> citasPorPropietario(PropietarioModel propietario) {
+        return agendaRepository.findByPropietario(propietario);
+    }
+
 }
 
-
-}
