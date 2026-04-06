04786fad40ae98b4709bccb90c2d4aa6c8a5e2d5
Daniel <sena.danielboss@gmail.com>
2026-04-06 02:06:52 -0500

    repository

--- src/main/java/scrum/cannia/repository/VeterinariaRepository.java
+++ src/main/java/scrum/cannia/repository/VeterinariaRepository.java
@@ -1,7 +1,10 @@
 package scrum.cannia.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import scrum.cannia.model.AgendaModel;
 import scrum.cannia.model.VeterinariaModel;
+
+import java.util.List;
 
 public interface VeterinariaRepository  extends JpaRepository<VeterinariaModel,Integer> {
 }
